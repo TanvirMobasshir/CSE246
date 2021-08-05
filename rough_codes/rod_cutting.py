e6bc6f2d3fe32aa4b73c1c948333ca1fee1eea04
tanvir <mdtanvirmobasshir@gmail.com>
2021-08-05 18:05:40 +0600

    rod_cutting done boiiiiiiii

--- rough_codes/rod_cutting.py
+++ rough_codes/rod_cutting.py
@@ -1,45 +1,24 @@
-<<<<<<< HEAD
-def rod(length: int, price: list):
-	print(dp)
-	if length == 0: return 0
-	if dp[length]: return dp[length]
-	mx = 0
-	i = 1
-	while i<=len(price) and length>=len(price):
-		val = price[i] + rod(length-i, price)
-		if val>mx: mx=val
-		i+=1
-		dp[length] = mx
-	return dp[length]
-=======
-def rod_cutting(length: int, price: list):
-    if len == 0: return 0
+def rod_cutting(length: int, price: list, cut_list: list):
+    if length < 0: return 0
     if dp[length]: return dp[length]
     mx = 0
-    i = 1
-    while i<=(length+1):
-        val = price[i]+rod_cutting(length-1, price)
-        dp[length] = val
-        if val>mx: mx = val
+    i = 0
+    while i <= length:
+        val = price[i] + rod_cutting(length - (i + 1), price, cut_list)
+        if val > mx:
+            dp[length] = mx = val
+            cut_list.append(length)
         i+=1
 
     return mx
->>>>>>> b12038fe5cd5e7828e3b621dacc1b115fba6ae1c
 
 
 if __name__ == "__main__":
 
-<<<<<<< HEAD
-	price = [0, 2, 8, 9, 10, 12]
-	length = 10
-	global dp
-	dp = [None] * (length+1)
-	print(rod(length, price))
-=======
-    length = 5
-    price = [0, 2, 8, 9, 10, 12]
-    global dp
-    dp = [None] * (length+1)
-    print(rod_cutting(length, price))
+    length = 8
+    price = [1, 5, 8, 9, 10, 17, 17, 20]
+    global dp, cut_list
+    cut_list = []
+    dp = [None] * len(price)
+    print(rod_cutting(len(price) - 1, price, cut_list))
 
->>>>>>> b12038fe5cd5e7828e3b621dacc1b115fba6ae1c
